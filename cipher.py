234583059cc19974d291d8bfb2e17fb794b9bbdc
RiskierGriffin <tenenbaumallan@gmail.com>
2017-12-28 15:41:32 -0400

    Improves whitespace

--- cipher.py
+++ cipher.py
@@ -8,17 +8,17 @@ user_choice = input("Do you want to encrypt(e) or decrypt(d) this message? ")
 if (user_choice == "e"):
     for character in message:
         position = alphabet.find(character)
-        new_position = (position + key)%26
+        new_position = (position + key) % 26
         new_char = alphabet[new_position]
         new_message += new_char
-    print("Your new message is:",new_message)
+    print("Your new message is:", new_message)
 elif (user_choice == "d"):
     for character in message:
         position = alphabet.find(character)
-        new_position = (position - key)%26
+        new_position = (position - key) % 26
         new_char = alphabet[new_position]
         new_message += new_char
-    print("Your original message is:",new_message)
+    print("Your original message is:", new_message)
 else:
     print("Not a valid choice, terminating program")
     sys.exit()
